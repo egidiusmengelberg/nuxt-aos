c3290d73c942c4a425c0aad15204d23696c75266
BayBreezy <behon.baker@yahoo.com>
2023-09-02 12:00:27 -0500

    feat(module): pass options to init method of aos

--- src/module.ts
+++ src/module.ts
@@ -1,15 +1,27 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, addPlugin, createResolver } from "@nuxt/kit";
+import { name, version } from "../package.json";
+import { AosOptions } from "aos";
 
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  options?: AosOptions;
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'nuxt-aos',
+    name,
+    version,
+    configKey: "aos",
   },
-  defaults: {},
-  setup () {
-    const resolver = createResolver(import.meta.url)
+  defaults: {
+    options: {},
+  },
+  setup(options, nuxt) {
+    const resolver = createResolver(import.meta.url);
 
-    addPlugin(resolver.resolve('./runtime/plugin'))
-  }
-})
+    // Get runtimeConfig & add aos to it so we can access it in plugin
+    const config = useRuntimeConfig();
+    config.public.aos = options.options;
+
+    addPlugin(resolver.resolve("./runtime/plugin"));
+  },
+});
