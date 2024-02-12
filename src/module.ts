325e7cff417ba36967c8339aece8db6aea9e9852
Egidius Mengelberg <mengelberg@mengelberg.eu>
2024-02-12 14:12:42 +0100

    fix devtools

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,5 @@
 import { defineNuxtModule, addPlugin, createResolver } from "@nuxt/kit";
+import { addCustomTab } from '@nuxt/devtools-kit';
 import { name, version } from "../package.json";
 import type { AosOptions } from "aos";
 
@@ -38,20 +39,18 @@ export default defineNuxtModule<ModuleOptions>({
     });
 
     // add custom tab
-    nuxt.hook("devtools:customTabs", (tabs) => {
-      tabs.push({
-        // unique identifier
-        name,
-        // title to display in the tab
-        title: "AOS Docs",
-        // any icon from Iconify, or a URL to an image
-        icon: "https://media.licdn.com/dms/image/D4D12AQEYCFvOjhb8rQ/article-cover_image-shrink_600_2000/0/1679941246531?e=2147483647&v=beta&t=A3azwRlUOpX_tBCILj49HMVojFrvGUZO8vC_yBi_dkQ",
-        // iframe view
-        view: {
-          type: "iframe",
-          src: "https://michalsnik.github.io/aos/",
-        },
-      });
-    });
+    addCustomTab(() => ({
+      // unique identifier
+      name,
+      // title to display in the tab
+      title: "AOS Docs",
+      // any icon from Iconify, or a URL to an image
+      icon: "https://media.licdn.com/dms/image/D4D12AQEYCFvOjhb8rQ/article-cover_image-shrink_600_2000/0/1679941246531?e=2147483647&v=beta&t=A3azwRlUOpX_tBCILj49HMVojFrvGUZO8vC_yBi_dkQ",
+      // iframe view
+      view: {
+        type: "iframe",
+        src: "https://michalsnik.github.io/aos/",
+      },
+    }))
   },
 });
